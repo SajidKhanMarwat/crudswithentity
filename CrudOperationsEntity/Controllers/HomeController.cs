292c6b15db6ee3ec48c2bf7a0622329b652a7f71
SajidKhanMarwat <msajidmarwat@gmail.com>
2023-01-15 17:18:02 +0500

    tt

---
+++ CrudOperationsEntity/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using CrudOperationsEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOperationsEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentInfoEntities studentInfoEntities = new StudentInfoEntities();
+
+        public ActionResult Index()
+        {
+
+            var data = studentInfoEntities.Students.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CrudOperationsEntity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudOperationsEntity.MvcApplication" Language="C#" %>
